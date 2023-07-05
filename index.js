5d0658fb69d77327e18504acfe7b3fbc88034fd1
Ridoy Sharif <developersridoy@gmail.com>
2023-07-05 14:52:21 +0600

    blogs single apis created

--- index.js
+++ index.js
@@ -57,6 +57,14 @@ async function run() {
       res.send(result)
     })
 
+    // specific blog place find
+    app.get("/blogs/:id",async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const result = await blogsCollection.findOne(filter);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
